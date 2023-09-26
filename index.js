b7f7101482ab7632599aa6b955e2f0dcc4cc2d58
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-09-26 11:12:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+// // Code your solution here
+// function fuzzyMatch(drivers, letters) {
+//     return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+//   }
+
+  
+//   function findMatching(drivers, name) {
+//     return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+//   }
+  
+//   function matchName(drivers, name) {
+//     return drivers.filter(driver => driver.name === name);
+//   }
+  
+
+function findMatching(drivers, driverName){
+  return drivers.filter(names => names.toLowerCase() === driverName.toLowerCase());
+
+}
+
+function fuzzyMatch(drivers, driverName){
+  return drivers.filter(names => names.toLowerCase().startsWith(driverName.toLowerCase()));
+}
+  
+function matchName(drivers, driverName){
+  return drivers.filter(names => names.name === driverName);
+}
